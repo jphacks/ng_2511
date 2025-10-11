21666fcec1af88352fbd533b3f3c55e2dc00db40
CS-Kmkm <motegi.koshi@gmail.com>
2025-10-11 09:19:33 +0900

    [mod] config

--- frontend/diary/src/app/page.tsx
+++ frontend/diary/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -20,9 +20,6 @@ export default function Home() {
               src/app/page.tsx
             </code>
             .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
@@ -43,7 +40,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy now!
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
