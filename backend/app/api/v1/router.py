f93b647591c9e8f093e45826ae1fc07092cd5a42
Valzaak <hiroki_6378@outlook.jp>
2025-10-11 12:51:02 +0900

    #33 [mod] split files

---
+++ backend/app/api/v1/router.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from . import diaries
+
+api_router = APIRouter(prefix="/api/v1")
+api_router.include_router(diaries.router)
+# api_router.include_router(items.router)
+# api_router.include_router(images.router)
+# api_router.include_router(users.router)
